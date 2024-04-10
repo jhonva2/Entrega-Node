53875b9a5101c0fce0687b8978a37648d5edc9f6
javierRamosMartinez <j.ramos232000@gmail.com>
2024-04-10 10:52:00 +0200

    Añadiendo vercel.json

--- index.js
+++ index.js
@@ -1,24 +1,30 @@
-const express = require('express')
-const cors = require ("cors");
-    require("dotenv").config();
+const express = require("express");
+const cors = require("cors");
+require("dotenv").config();
 
-const {clientRouter} = require ("./api/clientes/clientes.router") 
-const{connectMongo} = require("./util/db") 
-const {notFoundHandler, errorHandler} = require("./api/middleware/error.midleware");   
-const {contractRouter} = require ("./api/contracts/contracts.router") 
-const {serviceRouter} = require ("./api/services/services.router") 
+const { clientRouter } = require("./api/clientes/clientes.router");
+const { connectMongo } = require("./util/db");
+const {
+  notFoundHandler,
+  errorHandler,
+} = require("./api/middleware/error.midleware");
+const { contractRouter } = require("./api/contracts/contracts.router");
+const { serviceRouter } = require("./api/services/services.router");
 
 const PORT = process.env.PORT || 3001;
 const app = express();
 
 app.use(cors());
 app.use(express.json());
-app.use(express.urlencoded({ extended: true })); 
- 
-connectMongo()
-app.use("/client",clientRouter)
-app.use("/contract",contractRouter)
-app.use("/service",serviceRouter)
+app.use(express.urlencoded({ extended: true }));
+
+connectMongo();
+app.get("/", (req, res) => {
+  res.send("hola");
+});
+app.use("/client", clientRouter);
+app.use("/contract", contractRouter);
+app.use("/service", serviceRouter);
 app.listen(PORT, () => {
-    console.log("Servidor iniciado en puerto: " + PORT);
-  });
+  console.log("Servidor iniciado en puerto: " + PORT);
+});
